2889f0dbec4176dd46dcb3aecbc02651b447b9af
Nab-88 <nicolas.bernard38460@gmail.com>
2017-11-27 16:04:38 +0100

    ajout des fichiers de compilation

---
+++ build/CMakeFiles/ensivideo.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/ensimag/lecteur_video_thread/src/ensitheora.c" "/home/ensimag/lecteur_video_thread/build/CMakeFiles/ensivideo.dir/src/ensitheora.c.o"
+  "/home/ensimag/lecteur_video_thread/src/ensivideo.c" "/home/ensimag/lecteur_video_thread/build/CMakeFiles/ensivideo.dir/src/ensivideo.c.o"
+  "/home/ensimag/lecteur_video_thread/src/ensivorbis.c" "/home/ensimag/lecteur_video_thread/build/CMakeFiles/ensivideo.dir/src/ensivorbis.c.o"
+  "/home/ensimag/lecteur_video_thread/src/oggstream.c" "/home/ensimag/lecteur_video_thread/build/CMakeFiles/ensivideo.dir/src/oggstream.c.o"
+  "/home/ensimag/lecteur_video_thread/src/stream_common.c" "/home/ensimag/lecteur_video_thread/build/CMakeFiles/ensivideo.dir/src/stream_common.c.o"
+  "/home/ensimag/lecteur_video_thread/src/synchro.c" "/home/ensimag/lecteur_video_thread/build/CMakeFiles/ensivideo.dir/src/synchro.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
